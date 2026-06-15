a11ef309e2c398727139470e3f0847788db63280
hadess1213 <hadess1213@gmail.com>
2026-06-16 01:59:00 +0900

    Update token.js

--- functions/token.js
+++ functions/token.js
@@ -1,9 +1,36 @@
 exports.handler = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      success: true,
-      message: "토큰 함수 연결 성공"
-    })
-  };
+  try {
+    const response = await fetch(
+      "https://api.tossinvest.com/oauth2/token",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type":
+            "application/x-www-form-urlencoded"
+        },
+        body: new URLSearchParams({
+          grant_type: "client_credentials",
+          client_id: process.env.TOSS_CLIENT_ID,
+          client_secret: process.env.TOSS_CLIENT_SECRET
+        })
+      }
+    );
+
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+
+  } catch (e) {
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: e.message
+      })
+    };
+
+  }
 };
