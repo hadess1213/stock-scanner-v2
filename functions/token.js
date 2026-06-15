51812a459e315b16cfd8847dfbf3cec4461dbe0d
hadess1213 <hadess1213@gmail.com>
2026-06-16 02:04:18 +0900

    Update token.js

--- functions/token.js
+++ functions/token.js
@@ -1,36 +1,18 @@
 exports.handler = async () => {
   try {
-    const response = await fetch(
-      "https://api.tossinvest.com/oauth2/token",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type":
-            "application/x-www-form-urlencoded"
-        },
-        body: new URLSearchParams({
-          grant_type: "client_credentials",
-          client_id: process.env.TOSS_CLIENT_ID,
-          client_secret: process.env.TOSS_CLIENT_SECRET
-        })
-      }
-    );
-
-    const data = await response.json();
-
     return {
       statusCode: 200,
-      body: JSON.stringify(data)
+      body: JSON.stringify({
+        clientIdExists: !!process.env.TOSS_CLIENT_ID,
+        secretExists: !!process.env.TOSS_CLIENT_SECRET
+      })
     };
-
   } catch (e) {
-
     return {
       statusCode: 500,
       body: JSON.stringify({
         error: e.message
       })
     };
-
   }
 };
